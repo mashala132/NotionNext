0e4f6391b9fdf5159afef965efd7c104e5f1197b
mashala132 <skasgaardsoncalvin@gmail.com>
2024-09-03 19:54:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,9 +17,9 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: '',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '电报账号购买',
+  HEO_HERO_TITLE_LINK: 'https://1101204.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
