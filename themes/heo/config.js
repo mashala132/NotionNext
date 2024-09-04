825334b35cdb4acddfcce265ad0a4acf3330d2bf
mashala132 <skasgaardsoncalvin@gmail.com>
2024-09-04 13:53:30 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,11 +14,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '分享网络资源',
+  HEO_HERO_TITLE_2: '与网上冲浪技巧',
   HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '',
-  HEO_HERO_TITLE_5: '电报账号购买',
+  HEO_HERO_TITLE_5: '账号购买',
   HEO_HERO_TITLE_LINK: 'https://1101204.com',
 
   // 英雄区显示三个置顶分类
