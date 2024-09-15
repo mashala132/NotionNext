179031be451030b77a88563d896a2d998f5e9586
mashala132 <skasgaardsoncalvin@gmail.com>
2024-09-15 12:00:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,8 +21,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与网上冲浪技巧',
   HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '',
-  HEO_HERO_TITLE_5: '账号购买',
-  HEO_HERO_TITLE_LINK: 'https://1101204.com',
+  HEO_HERO_TITLE_5: '',
+  HEO_HERO_TITLE_LINK: 'https://1101204.xyz',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/推荐' },
